e9db35005bdb2678dfc9b89a9a94577e09a2b214
wooniwinnie <wooniwinnie@gmail.com>
2023-05-27 23:13:20 +0900

    Counter 상태 끌어올리기 구현

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AppProfile from './AppProfile';
+import AppCounter from './AppCounter';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <AppProfile />
+        <AppCounter />
     </React.StrictMode>
 );
 
