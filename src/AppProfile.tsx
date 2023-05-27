7ba6b09fbcba70f8c46a555570f532bc3353560b
wooniwinnie <wooniwinnie@gmail.com>
2023-05-27 19:02:02 +0900

    .

--- src/AppProfile.tsx
+++ src/AppProfile.tsx
@@ -1,10 +1,16 @@
+import { FormEvent } from 'react';
 import './App.css';
 
 import Profile from './components/Profile';
 
 export default function AppProfile() {
+    const handleClick = (e: FormEvent<HTMLButtonElement>) => {
+        console.log(e);
+        alert('버튼이 클리됨');
+    };
     return (
         <div>
+            <button onClick={handleClick}>클릭</button>
             <div className='container'>
                 <Profile
                     image='https://images.unsplash.com/photo-1522556189639-b150ed9c4330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'
